4c57409f7f737a7f12d3b06d99f7bded90bc358d
Ramon Pacheco <rmpacheco2@gmail.com>
2021-05-17 06:52:45 -0300

    add: transaction class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:julius/model/transaction.dart';
 
 main() => runApp(JuliusApp());
 
@@ -12,15 +13,40 @@ class JuliusApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Novo Tênis de Corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.36,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-      ),
-      body: Center(
-        child: Text('Versão Inicial'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              child: Text('Lista de Transações'),
+            )
+          ],
+        ));
   }
 }
