a49eb88f387aeac80a45e20364189fe1f368dba5
Ramon Pacheco <rmpacheco2@gmail.com>
2021-05-18 06:46:30 -0300

    improve home body list

--- lib/main.dart
+++ lib/main.dart
@@ -43,8 +43,12 @@ class HomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              child: Text('Lista de Transações'),
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Text(tr.title),
+                );
+              }).toList(),
             )
           ],
         ));
