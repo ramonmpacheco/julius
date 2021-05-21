0c25545c210b764a555f1f35abdf9b7b200e8c8e
Ramon Pacheco <rmpacheco2@gmail.com>
2021-05-20 21:38:50 -0300

    refact: create components

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:julius/components/transaction_form.dart';
+import 'package:julius/components/transaction_list.dart';
+import 'package:julius/models/transaction.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Novo Tênis de Corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.36,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
