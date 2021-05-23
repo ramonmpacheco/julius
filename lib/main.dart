2f98b3764c3e446e036aa8107029bd6bc05f861f
Ramon Pacheco <rmpacheco2@gmail.com>
2021-05-23 12:04:27 -0300

    add: close modal when add a transaciton

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,8 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
   _openTransactionFormModal(BuildContext context) {
